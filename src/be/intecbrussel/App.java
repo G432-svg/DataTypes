2d1219e03e9d8ee4daf76f2dab669038bb3fee73
Otilia <syo68mani@yahoo.com>
2021-06-02 20:21:17 +0200

    Initial commit

---
+++ src/be/intecbrussel/App.java
@@ -0,0 +1,28 @@
+package be.intecbrussel;
+
+
+public class DataTypesApp {
+
+
+    public static void main(String[] args) {
+        byte b = 52;
+        short s = 456;
+        int i = 456_799_8;
+        long l = 456_456_456L;
+        float f = 456.45F;
+        double d = 654.3D;
+        char c ='T';
+        boolean bln = true;
+
+        System.out.println("byte");
+        System.out.println("short");
+        System.out.println("int");
+        System.out.println("long");
+        System.out.println("float");
+        System.out.println("double");
+        System.out.println("char");
+        System.out.println("boolean");
+
+
+    }
+}
---
+++ src/be/intecbrussel/ClassTime.java
@@ -0,0 +1,25 @@
+package be.intecbrussel;
+
+import java.util.Scanner;
+
+public static ClassTime {
+
+        String dateStart = ("yy/MM/dd HH:mm:ss");
+        String dateStop = ("yy/MM/dd HH:mm:ss");
+
+        for(H=0;H<=23.59;H++);
+
+
+
+
+
+     String dateStart ="yy/MM/dd HH:mm:ss";
+       String dateStop ="yy/MM/dd HH:mm:ss";
+
+       for(H=0;H<=23.59;H++);
+
+
+
+
+}
+
